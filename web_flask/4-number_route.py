c2903603dbf97d9a0ef5d1b97e7b44040b6a54ab
khalilBahi <7753@holbertonstudents.com>
2024-04-11 16:08:50 +0200

    display n is a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,7 +30,7 @@ def display_python(text="is cool"):
     return f"Python {text.replace('_', ' ')}"
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def display_number(n):
     """number path"""
     return f"{n} is a number"
